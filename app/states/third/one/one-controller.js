f8dc23c49ea9070aa6fd7241f7bfb560275061d0
zeMirco <mirco.zeiss@gmail.com>
2014-05-16 19:47:37 +0200

    show example for controller inheritance with

--- app/states/third/one/one-controller.js
+++ app/states/third/one/one-controller.js
@@ -1,5 +1,7 @@
 'use strict';
 
+goog.require('my.third.Ctrl');
+
 /**
  * Create namespace.
  */
@@ -10,15 +12,31 @@ goog.provide('my.third.one.Ctrl');
 /**
  * third.one controller.
  *
+ * @param {angular.$controller} $controller
  * @constructor
  * @export
+ * @ngInject
  */
-my.third.one.Ctrl = function() {
+my.third.one.Ctrl = function($controller) {
 
   /**
    * @type {String}
    * @expose
    */
   this.label = 'some other label from third.one controller';
+
+  /**
+   * Inherit from parent controller. We'd like to call the `log`
+   * function here.
+   *
+   * @type {Object}
+   * @expose
+   */
+  this.parent = $controller(my.third.Ctrl);
+
+  /**
+   * Call parent `log` function.
+   */
+  this.parent.log('`log` function called from child controller');
 
 };
