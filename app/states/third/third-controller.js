bbba12581858ab9aa1ff031d1664d1b9c9c7a9ce
zeMirco <mirco.zeiss@gmail.com>
2014-05-16 19:48:17 +0200

    add example log function to parent controller

--- app/states/third/third-controller.js
+++ app/states/third/third-controller.js
@@ -22,3 +22,17 @@ my.third.Ctrl = function() {
   this.label = 'some label from third controller';
 
 };
+
+
+
+/**
+ * Write `text` to stdout.
+ *
+ * Example function that we'd like to access in our `third.one`
+ * child controller.
+ *
+ * @param {String} text
+ */
+my.third.Ctrl.prototype.log = function(text) {
+  console.log(text);
+};
